7c894a38f9abc676b315b914f2ead22d51fe5d62
sza-man <szymcube@gmail.com>
2024-01-11 18:18:36 +0100

    x

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("Hello World Siemanko Swiat");
+const calc = (a) => {
+    console.log(a * a);
+
+}
+
+
+
+
+
+let dupsko = 4;
+let Krystian = 100;
+var Szymon = 2;
+
+
+
+
+calc(dupsko);
+calc(3);
+calc(Krystian);
+calc(Szymon);
+
+let samochod = {
+    color: "red",
+    length: 25,
+}
+
+calc(samochod.length);
+calc(samochod["length"]);
+
+var samochod2 = ["blue",12]
+
+calc(samochod2[1]);
+
